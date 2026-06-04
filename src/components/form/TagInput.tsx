54544e0cf812b35cf9d2fcc024de16fe72d12c6e
calycekr <4094424+calycekr@users.noreply.github.com>
2026-06-05 02:14:18 +0900

    feat(form): support success validation feedback on tags and textarray inputs

--- src/components/form/TagInput.tsx
+++ src/components/form/TagInput.tsx
@@ -65,6 +65,7 @@ export const FormItemTagsInput = <T extends FieldValues, R>(
       label={label}
       description={description}
       error={fieldState.error?.message}
+      status={fieldState.isDirty && !fieldState.error ? 'success' : undefined}
       fieldPath={controllerProps.name}
       required={!!controllerProps.rules?.required}
     >
